67d6fd7e2083af58062074f80fd2861fded7b1bb
2betop <liaoxuezhi@icloud.com>
2015-04-17 10:00:46 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -3,4 +3,12 @@
 // });
 
 // 开起 autuload, 好处是，依赖自动加载。
-fis.config.set('modules.postpackager', 'autoload');
+fis.config.set('modules.postpackager', fis.plugin('autoload'));
+
+fis.match('components/**/*.js', {
+  packTo: 'pkg/components.js'
+});
+
+fis.match('components/**/*.css', {
+  packTo: 'pkg/components.css'
+});
