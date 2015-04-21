9ecb8a75ff24f690047322266ae7c60c79a34548
2betop <liaoxuezhi@icloud.com>
2015-04-21 13:54:23 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,9 +1,8 @@
-// fis.match('components/**.js', {
-//   isMod: true
-// });
+// 以 html 文件作为入口，所有 html 文件以及与之关联的静态文件也会被自动编译，用了啥编译啥，所谓按需编译。
+fis.set('project.index', '*.html');
 
 // 开起 autuload, 好处是，依赖自动加载。
-fis.config.set('modules.postpackager', fis.plugin('autoload'));
+fis.set('modules.postpackager', fis.plugin('autoload'));
 
 fis.match('components/**/*.js', {
   packTo: 'pkg/components.js'
