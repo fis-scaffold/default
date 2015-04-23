00f71d19f49bb6c37de41bfc754e9fe67eb97943
2betop <liaoxuezhi@icloud.com>
2015-04-23 15:27:35 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,7 +1,28 @@
 // 以 html 文件作为入口，所有 html 文件以及与之关联的静态文件也会被自动编译，用了啥编译啥，所谓按需编译。
-fis.set('project.index', '*.html');
+fis.set('project.files', '*.html');
 
-// 开起 autuload, 好处是，依赖自动加载。
+// default settings
+fis
+  .env('dev')
+  .match('**', {
+    useHash: false,
+    useDomain: false
+  });
+
+// fis release --media production
+fis
+  .env('production')
+  .match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+  })
+  .match('*.css', {
+    optimizer: fis.plugin('clean-css')
+  })
+  .match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+  });
+
+// 开启 autuload, 好处是，依赖自动加载。
 fis.set('modules.postpackager', fis.plugin('autoload'));
 
 fis.match('components/**/*.js', {
