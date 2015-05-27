10b05a279863ca5c86f5ff2206dc017e5b7e9dff
2betop <liaoxuezhi@icloud.com>
2015-05-27 12:45:47 +0800

    udpate

--- fis-conf.js
+++ fis-conf.js
@@ -1,6 +1,3 @@
-// 以 html 文件作为入口，所有 html 文件以及与之关联的静态文件也会被自动编译，用了啥编译啥，所谓按需编译。
-fis.set('project.files', '*.html');
-
 // default settings. fis3 release
 fis
   .media('dev')
@@ -24,22 +21,4 @@ fis
 
   .match('*.png', {
     optimizer: fis.plugin('png-compressor')
-  })
-
-  .match('/components/**/*.js', {
-    packTo: '/pkg/components.js'
-  })
-
-  .match('/components/**/*.css', {
-    packTo: '/pkg/components.css'
   });
-
-// 开启 autuload, 好处是，依赖自动加载。
-fis.set('modules.postpackager', fis.plugin('autoload'));
-
-// 添加 scss 插件
-fis.match('*.scss', {
-  parser: fis.plugin('sass', {
-    include_paths: ['./static/scss']
-  })
-});
